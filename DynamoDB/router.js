85ee9f86ea534a869b0876b5cd9a75c17f915b15
sirajul116 <hridoyhrix116@gmail.com>
2024-05-17 23:37:03 +0600

    Implement GSI and GSI query

--- DynamoDB/router.js
+++ DynamoDB/router.js
@@ -4,10 +4,12 @@ const {
   createShop,
   removeShop,
   updateShop,
+  getDetail,
 } = require('./controllers/shop');
 
 const router = new Router();
 router.get('/shop/:id', getShopDetails);
+router.get('/shop/name/:sname', getDetail);
 router.post('/create', createShop);
 router.delete('/delete/:id', removeShop);
 router.put('/update', updateShop);
